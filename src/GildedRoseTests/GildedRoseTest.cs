7b3043d89bd6826b16c021e521c7713656bbf210
martin <martin.tierney86@hotmail.co.uk>
2025-02-15 18:12:53 +0000

    start of adding tests to document the reqs

--- src/GildedRoseTests/GildedRoseTest.cs
+++ src/GildedRoseTests/GildedRoseTest.cs
@@ -15,5 +15,25 @@ namespace GildedRoseTests
             app.UpdateQuality();
             Assert.Equal(itemName, Items[0].Name);
         }
+
+        [Fact]
+        public void GivenStandardItem_WhenTimePasses_SellInReduces()
+        {
+            const int expectedSellIn = -1;
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(expectedSellIn, Items[0].SellIn);
+        }
+
+        [Fact]
+        public void GivenStandardItem_WhenTimePasses_QualityReduces()
+        {
+            const int expectedQuality = 2;
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 3, Quality = 3 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(expectedQuality, Items[0].Quality);
+        }
     }
 }
