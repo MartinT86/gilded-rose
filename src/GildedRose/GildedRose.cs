089d4599ee306db01293ba9464be04feb52449fd
martin <martin.tierney86@hotmail.co.uk>
2025-02-15 20:19:06 +0000

    strangle sulfuras from if statement

--- src/GildedRose/GildedRose.cs
+++ src/GildedRose/GildedRose.cs
@@ -16,10 +16,14 @@ namespace GildedRoseKata
             Items.Where(x => x.Name == "Aged Brie").ToList().ForEach(x => BrieUpdater.Update(x));
             Items.Where(x => x.Name == "Backstage passes to a TAFKAL80ETC concert")
                 .ToList().ForEach(x => BackstagePassUpdater.Update(x));
+            Items.Where(x => x.Name == "Sulfuras, Hand of Ragnaros")
+                .ToList().ForEach(x => SulfurasUpdater.Update(x));
 
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (Items[i].Name != "Aged Brie" 
+                    && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert"
+                    && Items[i].Name != "Sulfuras, Hand of Ragnaros")
                 {
                     if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                     {
